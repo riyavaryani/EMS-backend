7e9d321503eb48b25d2e9cbb806dcc7eb7403ccd
riyavaryani <riyavaryani01@gmail.com>
2024-02-27 16:35:07 +0530

    cors issue fix

--- src/main/java/com/learnspring/ems/controller/EmployeeController.java
+++ src/main/java/com/learnspring/ems/controller/EmployeeController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@CrossOrigin(origins="http://127.0.0.1:3000")
 @AllArgsConstructor
 @RestController
 @RequestMapping("/api/employees")
